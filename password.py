01170c3dd94568a5833f1d51e4f4d34fa7e689ad
Anthonyc833 <82863945+Anthonyc833@users.noreply.github.com>
2021-08-13 10:13:26 +0800

    Update password.py

--- password.py
+++ password.py
@@ -1,4 +1,14 @@
->>> from random import *
->>> characters = string.ascii_letters + string.punctuation + string.digits
->>> password = "".join(choice(characters) for x in range(randint(8, 16)))
->>> print password
+import string
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+plenght = (int("how long is the password"))
+if plenght == 0:
+  print("you cant print that")
+else if plenght < 50:
+  print("thats to big")
+else:
+  password =  "".join(choice(characters) for x in range(plenght))
+
+
+
+print (password)
