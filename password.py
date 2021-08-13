06954439bea9f84c171451b6c8e63d1c22fce3a1
Anthonyc833 <82863945+Anthonyc833@users.noreply.github.com>
2021-08-13 10:38:50 +0800

    Update password.py

--- password.py
+++ password.py
@@ -3,13 +3,25 @@ import random
 from random import *
 characters = string.ascii_letters + string.punctuation  + string.digits
 plenght = input("how long is the password")
-if plenght == 0:
+Upperletters = input("do you want upper case")
+Lowerletters=input("do you want lower case letters")
+digits=input("any numbers in that")
+characters2=input("special characters")
+if Upperletters == "Y":
+  UL="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+else:
+  UL=""
+if Lowerletters == "Y":
+  LL= "abcdefghijklmnopqrstuvwxyz"
+else:
+  LL=""
+
+
+
+if int(plenght) < 0:
   print("you cant print that")
-else if plenght < 50:
+elif int(plenght) > 50:
   print("thats to big")
 else:
-  password =  "".join(choice(characters) for x in range(plenght))
-
-
-
-print (password)
+  password =  "".join(choice(characters) for x in range(int(plenght)))
+  print (password)
